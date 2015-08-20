467b1932c6108e399d2017cb74e406c48b8618ce
Henry Conklin <henrywconklin@gmail.com>
2015-08-20 11:53:44 -0400

    Added remote deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/pi/barkdetect/
+
+eval `ssh-agent -s`
+
+# Pull the code from the git repository
+git pull
+
+# Update cron jobs, taken from:
+# http://stackoverflow.com/questions/2367824/storing-crontab-file-inside-my-project
+test -f barkdetect.crontab && { { crontab -u pi -l | awk '/^#BEGIN BARKDETECT/{hide=1;next}/^#END BARKDETECT/{hide=0;next}!hide{print}' ; echo '#BEGIN BARKDETECT' ; cat barkdetect.crontab ; echo '#END BARKDETECT' ; } | crontab -u pi -; }
+
+
+cd /
